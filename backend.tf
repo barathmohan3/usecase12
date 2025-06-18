aaf74de047e13a3d6ea80da22bd285df063efc7d
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-18 12:13:06 +0530

    Update backend.tf

---
+++ backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket         = "my-oidc-bucket-15328069840"
+    key            = "uc-12/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    use_lockfile   = false
+    dynamodb_table = "bmware-table"
+  }
+}
