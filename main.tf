f63cae5d71ce8d612a2c701b5760031c2feaf747
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-18 17:41:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,3 +17,12 @@ module "rds" {
   database_name        = var.database_name
   rds_security_group_ids  = [module.sg_group.rds_security_group_aurora_id]
 }
+
+
+# test aws cli to check connection of aurora database 
+module "instances_test" {
+  source          = "./modules/instance"
+  subnet_id       = module.vpc.public_subnets[0]
+  vpc_security_group_ids = [module.sg_group.ec2_security_group_id]
+  name            = "instance-test"
+}
