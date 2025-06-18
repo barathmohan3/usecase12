df8b5ed336bfb8e4930e7516aabf08d0f92a7f28
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-18 14:10:52 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,29 @@
-variable "region" {}
-variable "db_username" {}
-variable "db_password" {}
-variable "key_name" {}
-variable "security_group_id" {}
-variable "subnet_id_1" {}
-variable "subnet_id_2" {}
-variable "ec2_subnet_id" {}
+variable "vpc_cidr_block" {
+  description = "Cidr range for vpc"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "name" {
+  description = "Name to be used on VPC created"
+  type        = string
+  default     = "test" 
+}
+
+variable "region" {
+  description = "Name of the region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "db_username" {
+  description = "Name of the db_username"
+  type        = string
+  default     = "admin"
+}
+
+variable "database_name" {
+  description = "Name of the db_name"
+  type        = string
+  default     = "test"
+}
