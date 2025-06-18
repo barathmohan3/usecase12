00d687421611d9b6bf165be9fa35195fdbaa9d9f
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-18 17:55:21 +0530

    Update main.tf

--- modules/sg_group/main.tf
+++ modules/sg_group/main.tf
@@ -22,3 +22,8 @@ output "rds_security_group_aurora_id" {
   description = "Security group ID for the RDS"
   value       = aws_security_group.rds_sg.id 
 }
+
+output "ec2_security_group_id" {
+  description = "Security group ID for EC2 instances"
+  value       = aws_security_group.ec2_sg.id
+}
