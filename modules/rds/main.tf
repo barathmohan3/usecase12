f11314def51d6df593f9e9d27e4d8c6d2c3cef2c
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-18 14:11:49 +0530

    Update main.tf

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -1,24 +1,39 @@
-resource "aws_db_subnet_group" "default" {
+resource "aws_db_subnet_group" "subnet_group" {
   name       = "aurora-subnet-group"
-  subnet_ids = [
-    var.subnet_id_1,
-    var.subnet_id_2
-  ]
+  subnet_ids = var.private_subnets
 }
 
-resource "aws_rds_cluster" "aurora" {
-  cluster_identifier      = "aurora-cluster"
-  engine                  = "aurora-mysql"
-  master_username         = var.db_username
-  master_password         = var.db_password
-  skip_final_snapshot     = true
-  database_name           = "sampledb"
-  vpc_security_group_ids  = [var.security_group_id]
+resource "aws_rds_cluster" "aurora_mysql" {
+  cluster_identifier           = "aurora-mysql-cluster"
+  engine                       = "aurora-mysql"
+  engine_version               = "8.0.mysql_aurora.3.04.0"
+  database_name                = var.database_name
+
+  db_subnet_group_name         = aws_db_subnet_group.subnet_group.name
+  vpc_security_group_ids       = var.rds_security_group_ids
+
+  manage_master_user_password  = true
+  master_username              = var.db_username
+  skip_final_snapshot          = true
 }
 
-resource "aws_rds_cluster_instance" "aurora_instance" {
-  identifier              = "aurora-instance-1"
-  cluster_identifier      = aws_rds_cluster.aurora.id
-  instance_class          = "db.t3.medium"
-  engine                  = aws_rds_cluster.aurora.engine
+
+
+resource "aws_rds_cluster_instance" "aurora_instances" {
+  count                     = 1
+  identifier                = "aurora-instance-${count.index}"
+  cluster_identifier        = aws_rds_cluster.aurora_mysql.id
+  instance_class            = "db.t3.medium"
+  engine                    = aws_rds_cluster.aurora_mysql.engine
+  db_subnet_group_name      = aws_db_subnet_group.subnet_group.name
+  publicly_accessible       = false
+  
 }
+
+output "aurora_cluster_endpoint" {
+  value = aws_rds_cluster.aurora_mysql.endpoint
+}
+
+#output "aurora_cluster_instance_endpoint" {
+#  value = aws_rds_cluster.aurora_instances.endpoint
+#}
